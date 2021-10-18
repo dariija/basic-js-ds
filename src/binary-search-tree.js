1aeb4bcc01fde6c55ed22e9efb7e536f5dea99c3
dariija <darija.bakanova@gmail.com>
2021-10-18 05:48:46 +0300

    add solution for binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -8,39 +8,141 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 module.exports = class BinarySearchTree {
 
+  constructor() {
+    this.initialRoot = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.initialRoot
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    function createNode(data) {
+        let node = {
+          data: data,
+          leftChild: null,
+          rightChild: null
+        };
+        return node
+    }
+
+    if (!this.initialRoot) {
+      this.initialRoot = createNode(data);
+      return
+    }
+
+    let currentNode = this.initialRoot;
+
+    while (currentNode) {
+      if (data < currentNode.data) {
+        if (!currentNode.leftChild) {
+          currentNode.leftChild = createNode(data);
+          break
+        } else {
+          currentNode = currentNode.leftChild
+        }
+      } else if (data > currentNode.data) {
+        if (!currentNode.rightChild) {
+          currentNode.rightChild = createNode(data);
+          break
+        } else {
+          currentNode = currentNode.rightChild
+        }
+      } else break
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let currentNode = this.initialRoot;
+
+    while (currentNode) {
+      if (data < currentNode.data) {
+        currentNode = currentNode.leftChild;
+      } else if (data > currentNode.data) {
+        currentNode =  currentNode.rightChild;
+      } else {
+        return true
+      }
+    };
+    return false
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let currentNode = this.initialRoot;
+
+    while (currentNode) {
+      if (data < currentNode.data) {
+        currentNode = currentNode.leftChild
+      } else if (data > currentNode.data) {
+        currentNode = currentNode.rightChild
+      } else {
+        return currentNode
+      }
+    };
+    return null
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.initialRoot = removeNode(this.initialRoot, data);
+  
+    function removeNode(node, data) {
+      if(!node) return null;
+  
+      if (data < node.data) {
+        node.leftChild = removeNode(node.leftChild, data);
+        return node;
+      } else if (data > node.data) {
+        node.rightChild = removeNode(node.rightChild, data)
+        return node;
+      } else {
+        if (!node.leftChild && !node.rightChild) {
+          return null
+        };
+
+        if (!node.leftChild) {
+          node = node.rightChild;
+          return node;
+        };
+        
+        if (!node.rightChild) {
+          node = node.rightChild;
+          return node;
+        };
+
+        let min = node.rightChild;
+        while (min.leftChild) {
+          min = min.leftChild;
+        };
+        node.data = min.data;
+        node.rightChild = removeNode(node.rightChild, min.data);
+        return node
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.initialRoot) return null;
+
+    let currentNode = this.initialRoot;
+    while(currentNode) {
+      if (currentNode.leftChild) {
+        currentNode = currentNode.leftChild
+      } else {
+        return currentNode.data
+      }
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    if(!this.initialRoot) return null;
 
+    let currentNode = this.initialRoot;
+    while (currentNode) {
+      if (currentNode.rightChild) {
+        currentNode = currentNode.rightChild
+      } else {
+        return currentNode.data
+      }
+    }
+  }
 }
